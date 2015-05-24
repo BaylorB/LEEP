8eaf21b0abfe385f355bda01596fa5ae407c7047
kevinteich <kevinteich@gmail.com>
2015-05-24 17:22:00 -0400

    Added Neopixel rainbow(), call it on startup

--- LEEP.ino
+++ LEEP.ino
@@ -119,6 +119,8 @@ void setup()
   strip.show();
 
   delay(100);
+  
+  rainbow(20);
 }
 
 void loop() 
@@ -210,6 +212,29 @@ void loop()
     }
   }
   strip.show();
+}
+
+uint32_t Wheel(byte WheelPos) {
+  if(WheelPos < 85) {
+   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+  } else if(WheelPos < 170) {
+   WheelPos -= 85;
+   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+  } else {
+   WheelPos -= 170;
+   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+  }
+}
+
+void rainbow(uint8_t wait) {
+  uint16_t i, j;
+  for(j=0; j<256; j++) {
+    for(i=0; i<strip.numPixels(); i++) {
+      strip.setPixelColor(i, Wheel((i+j) & 255));
+    }
+    strip.show();
+    delay(wait);
+  }
 }
 
 
