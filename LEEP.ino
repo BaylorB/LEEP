1be74efc1636bb7ca66bfa03dd2f88bc19734710
Lee Keyser-Allen <lkeyser@alum.wpi.edu>
2015-10-03 16:37:42 -0400

    Changed the infratructure around to support better game development
    Added board-test 'game' with slightly changed colors from checked in version.
    Added game switching mechanism (long-press button closest to plug)
    
    Testing:
    Able to perform previous game as expected, but also able to switch games by
    holding down the button. Right now, it just switches back to the only game, but...

--- LEEP.ino
+++ LEEP.ino
@@ -1,240 +1,514 @@
+// Includes
+#include <Adafruit_NeoPixel.h>
+
 ////////////////////////
 // DEFINED CONSTANTS////
 ////////////////////////
 #define NUM_ROWS         4
 #define NUM_COLS         4
 
-#define NUM_NEOPIXELS    NUM_ROWS * NUM_COLS
+#define NUM_NEOPIXELS    (NUM_ROWS * NUM_COLS)
 
-#define SERIAL9600
+#define COLOR_NONE      0x000000
+#define RED             0xFF0000
+#define GREEN           0x00FF00
+#define BLUE            0x0000FF
+#define CYAN            0x00FFFF
+#define MAGENTA         0xFF00FF
+#define YELLOW          0xFFFF00
 
-//#define DEBUG 1
+#define LIGHT_BLUE      0x1772FF
+#define LIGHT_GREEN     0x0AFF4F
+
+#define LONG_PRESS_MS   1000
+
+// Blue Board LED
+const int ledPin = 13;
+
+// This class manages the button board.
+class ButtonHandler
+{
+public:
+  ButtonHandler()
+  : m_buttons() // initialize the buttons to off/up.
+  , m_nMaxStates(2)
+  {
+  }
+
+  void initialize()
+  {
+    // set Digital Pins 9-12 as OUTPUTS
+    for(uint8_t col = 0; col < NUM_COLS; col++)
+    {
+      pinMode(ms_colToPinNumber[col], OUTPUT);
+    }
+  
+    // set Digital Pins 5-8 (IC Pins 11-14) as INPUTS pulled high
+    for(uint8_t row = 0; row < NUM_ROWS; row++)
+    {
+      pinMode(ms_rowToPinNumber[row], INPUT_PULLUP);
+    }
+    
+    // all columns (outputs) are set high to start
+    for(uint8_t col = 0; col < NUM_COLS; col++)
+    {
+      digitalWrite(ms_colToPinNumber[col], HIGH);
+    }
+  }
+
+  void reset()
+  {
+    for (uint8_t row = 0 ; row < NUM_ROWS ; ++row)
+    {
+      for (uint8_t col = 0 ; col < NUM_COLS ; ++col)
+      {
+        m_buttons[row][col].pressed = false;
+        m_buttons[row][col].state = 0;
+      }
+    }
+  }
+
+  void setNumStates(uint8_t nNumStates)
+  {
+    m_nMaxStates = nNumStates;
+    reset();
+  }
+
+  bool updateButtonState()
+  {
+    bool bButtonStateChanged = false;
+
+    // scan across columns - make each column go low, one at a time
+    for(uint8_t col = 0; col < NUM_COLS; col++)
+    {
+      // set the column low
+      digitalWrite(ms_colToPinNumber[col], LOW);
+      // wait for the voltage to stabilize
+      delay(5);  //button reaction delay//
+  
+      // scan across rows testing switches and putting data in array
+      for (int row = 0; row < NUM_ROWS; row++)
+      {
+        // Read the pin (check switch) -- we're pressed if switchData is 0.
+        bool bPressed = !digitalRead(ms_rowToPinNumber[row]);
+
+        // if the state has changed
+        if (m_buttons[row][col].pressed != bPressed)
+        {
+          // Update button pressed state. 
+          m_buttons[row][col].pressed = bPressed;
+          // if it's going from up to down
+          if (bPressed)
+          {
+            // on the press, let's update our button to flip.
+            if (++m_buttons[row][col].state >= m_nMaxStates) {
+              m_buttons[row][col].state = 0;
+            }
+          }
+          // note that one of our buttons has changed state.
+          bButtonStateChanged = true;
+        }
+      }
+      
+      // raise column back HIGH
+      digitalWrite(ms_colToPinNumber[col], HIGH);
+    }
+    return bButtonStateChanged;
+  }
+
+  bool isButtonPressed(uint8_t row, uint8_t col)
+  {
+//    assert(row < NUM_ROWS);
+//    assert(col < NUM_COLS);
+    return m_buttons[row][col].pressed;
+  }
+
+  bool isButtonOn(uint8_t row, uint8_t col)
+  {
+//    assert(row < NUM_ROWS);
+//    assert(col < NUM_COLS);
+    return m_buttons[row][col].state;
+  }
+
+  uint8_t buttonState(uint8_t row, uint8_t col)
+  {
+//    assert(row < NUM_ROWS);
+//    assert(col < NUM_COLS);
+    return m_buttons[row][col].state;
+  }
+
+private:
+  // Button struct for keeping track of button/light state.
+  typedef struct
+  {
+    bool pressed;
+    uint8_t state;
+  } Button;
+  
+  Button m_buttons [NUM_ROWS][NUM_COLS];
+  static const uint8_t ms_rowToPinNumber[];
+  static const uint8_t ms_colToPinNumber[];
+  uint8_t m_nMaxStates;
+} g_buttons;
 
 // The input wires for the rows and columns of the grid. Top row is purple striped wire pin 12,
 // left column is brown wire pin 8.
-byte rowToPinNumber[NUM_ROWS] = {9, 10, 11, 12};
-byte colToPinNumber[NUM_COLS] = {5, 6, 7, 8}; 
-
-// LED that indicates when key is pressed
-const int ledPin = 13;
-
-// Button struct for keeping track of button/light state.
-typedef struct
-{
-  boolean pressed;
-  boolean highlight;
-  boolean handled;
-} Button;
-Button buttons [NUM_ROWS][NUM_COLS];
-
+const uint8_t ButtonHandler::ms_rowToPinNumber[] = {9, 10, 11, 12};
+const uint8_t ButtonHandler::ms_colToPinNumber[] = {5, 6, 7, 8}; 
 
 #define NEO_PIN 4
-#include <Adafruit_NeoPixel.h>
-Adafruit_NeoPixel strip = Adafruit_NeoPixel(NUM_NEOPIXELS, NEO_PIN, NEO_GRB + NEO_KHZ800);
-
-// colors
-uint32_t ledColor = 0;
-uint32_t ledColorNone = 255;
-uint32_t ledHighlightColor = 255;
-uint32_t ledHighlightOnColor = 255;
-
-
-int colRowToNeopixelIndex(int col, int row)
+class LedHandler
 {
-#ifdef DEBUG
-  Serial.print("colRowToNeopixelIndex ");
-  Serial.print(col);
-  Serial.print(" ");
-  Serial.print(row);
-#endif
- 
-  int index = 0;
-  index = row + ((NUM_COLS-1 - col) * NUM_ROWS);
+public:
+  LedHandler()
+  : m_strip(NUM_NEOPIXELS, NEO_PIN, NEO_GRB | NEO_KHZ800)
+  {
+    
+  }
+  ~LedHandler()
+  {
+    
+  }
+
+  void initialize()
+  {
+    m_strip.begin();
+    setAllPixels(0x0);
+  }
+
+  void beginUpdate()
+  {
+    // do nothing here for now
+  }
   
-#ifdef DEBUG
-  Serial.print(" - index ");
-  Serial.println(index);
-#endif
+  void setPixelColor(uint8_t row, uint8_t col, uint32_t color)
+  {
+    uint16_t index = colRowToNeopixelIndex(col,row);
+    m_strip.setPixelColor(index, color);
+  }
 
-  return index;
-}
+  void setAllPixels(uint32_t color)
+  {
+    for(uint16_t neopixel = 0; neopixel < NUM_NEOPIXELS; neopixel++)
+    {
+      m_strip.setPixelColor(neopixel, color);
+    }
+    m_strip.show();
+  }
 
+  void endUpdate()
+  {
+    // REVIEW: Should we make this conditional? (I don't think so, but TBD)
+    m_strip.show();
+  }
+private:
 
+Adafruit_NeoPixel m_strip;
+
+  uint16_t colRowToNeopixelIndex(uint8_t col, uint8_t row)
+  {
+    uint16_t index = 0;
+    index = row + ((NUM_COLS - 1 - col) * NUM_ROWS);
+    
+    return index;
+  }
+} g_leds;
 
 void setup() 
 {
   // initialize serial port
   Serial.begin(9600);
-  
-  // set Digital Pins 9-12 as OUTPUTS
-  for(int col = 0; col < NUM_COLS; col++)
-  {
-    pinMode(colToPinNumber[col], OUTPUT);
-#ifdef DEBUG
-    Serial.print("digital pin ");
-    Serial.print(colToPinNumber[col]);
-    Serial.println(" set as output");
-#endif
-  }
-
-  // set Digital Pins 5-8 (IC Pins 11-14) as INPUTS pulled high
-  for(int row = 0; row < NUM_ROWS; row++)
-  {
-    pinMode(rowToPinNumber[row], INPUT_PULLUP);
-#ifdef DEBUG
-    Serial.print("digital pin ");
-    Serial.print(rowToPinNumber[row]);
-    Serial.println(" set as input");  
-#endif
-  }
-  
   // initialize digital pin as an output (for LED)
   pinMode(ledPin, OUTPUT);
 
-
-  // Initialize button data.
-  for(int row = 0; row < NUM_ROWS; row++)
-  {
-    for(int col = 0; col < NUM_COLS; col++)
-    {
-      buttons[row][col].pressed = false;
-      buttons[row][col].highlight = false;
-      buttons[row][col].handled = false;
-    }
-  }
-  
-
-  // Initialize NeoPixel colors
-  ledColor = strip.Color(0, 255, 0); // not used
-  ledColorNone = strip.Color(0, 0, 0);
-  ledHighlightColor = strip.Color(23, 114, 255);  // Green //
-  ledHighlightOnColor = strip.Color(10, 255, 79); // blue // 
-
+  // Setup Buttons
+  g_buttons.initialize();
   // Initialize NeoPixels
-  strip.begin();
-  for(int neopixel = 0; neopixel < NUM_NEOPIXELS; neopixel++)
-  {
-    strip.setPixelColor(neopixel, ledColorNone);
-  }
-  strip.show();
+  g_leds.initialize();
 
   delay(100);
   
-  rainbow(20);
-}
-
-void loop() 
-{ 
-  // all columns (outputs) are set high to start
-  for(int col = 0; col < NUM_COLS; col++)
-  {
-    digitalWrite(colToPinNumber[col], HIGH);
-  }
-  // light LED as we start reading data
+  rainbow(2);
+  // light LED to show we've booted.
   digitalWrite(ledPin, HIGH);
+}
 
-  // scan across columns - make each column go low, one at a time
-  for(int col = 0; col < NUM_COLS; col++)
+class BoardTest
+{
+public:
+  void setup()
   {
-    digitalWrite(colToPinNumber[col], LOW);
-    
-    // scan across rows testing switches and putting data in array
-    for(int row = 0; row < NUM_ROWS; row++)
-    {
-      // "Read" the voltage present on the pin (check switch)
-      int switchData = digitalRead(rowToPinNumber[row]);
-
-      // Update button pressed state. We're pressed if switchData is 0.
-      buttons[row][col].pressed = (switchData == 0);
-      
-#ifdef DEBUG
-      if(switchData == 0)
-      {
-        Serial.print("Pressing col row ");
-        Serial.print(col);
-        Serial.print(" ");
-        Serial.println(row);
-      }
-#endif
-      
-      delay(5);  //button reaction delay//
-    }
-    
-    // raise column back HIGH
-    digitalWrite(colToPinNumber[col], HIGH);
+    g_buttons.setNumStates(2);
   }
+
+  // Basic board test -- lights go on when the button is pressed and go off
+  // when they are pressed again.
+  void loop()
+  {
+    g_leds.beginUpdate();
+    // Update our lights.
+    for (uint8_t row = 0; row < NUM_ROWS; row++)
+    {
+      for (uint8_t col = 0; col < NUM_COLS; col++)
+      {
+        uint32_t color = COLOR_NONE;
         
-  // turn off LED - done reading data
-  digitalWrite(ledPin, LOW);
+        if (g_buttons.isButtonPressed(row,col))
+        {
+          color = MAGENTA;
+        }
+        else if (g_buttons.isButtonOn(row,col))
+        {
+          color = BLUE;
+        }
   
-  // use to test loop speed
-#ifdef DEBUG
-  delay(1000);
-#endif
+        g_leds.setPixelColor(row, col, color);
+      }
+    }
+    g_leds.endUpdate();
+  }
+} g_boardTest;
 
-  // Update our lights.
-  for (int row = 0; row < NUM_ROWS; row++)
+class Drawing
+{
+  void setup()
   {
-    for(int col = 0; col < NUM_COLS; col++)
+    g_buttons.setNumStates(7);
+  }
+
+  // Basic board test -- lights go on when the button is pressed and go off
+  // when they are pressed again.
+  void loop()
+  {
+    g_leds.beginUpdate();
+    // Update our lights.
+    for (uint8_t row = 0; row < NUM_ROWS; row++)
     {
-      uint32_t color = 0;
-      
-      if(buttons[row][col].pressed)
+      for (uint8_t col = 0; col < NUM_COLS; col++)
       {
-        // We're being pressed, have we handled this press yet? If not
-        if(!buttons[row][col].handled)
+        uint32_t color = COLOR_NONE;
+        
+        if (g_buttons.isButtonPressed(row,col))
         {
-          // toggle highlight state.
-          buttons[row][col].highlight = !buttons[row][col].highlight;
-          
-          // Set our handled flag so we don't toggle again this press.
-          buttons[row][col].handled = true;
+          color = MAGENTA;
         }
-        color = ledHighlightOnColor;
+        else if (g_buttons.isButtonOn(row,col))
+        {
+          color = BLUE;
+        }
+  
+        g_leds.setPixelColor(row, col, color);
       }
-      else
+    }
+    g_leds.endUpdate();
+  }
+} g_drawing;
+
+//class BoardTest
+//{
+//  void setup()
+//  {
+//    g_buttons.setNumStates(2);
+//  }
+//
+//  // Basic board test -- lights go on when the button is pressed and go off
+//  // when they are pressed again.
+//  void loop()
+//  {
+//    g_leds.beginUpdate();
+//    // Update our lights.
+//    for (uint8_t row = 0; row < NUM_ROWS; row++)
+//    {
+//      for (uint8_t col = 0; col < NUM_COLS; col++)
+//      {
+//        uint32_t color = COLOR_NONE;
+//        
+//        if (g_buttons.isButtonPressed(row,col))
+//        {
+//          color = MAGENTA;
+//        }
+//        else if (g_buttons.isButtonOn(row,col))
+//        {
+//          color = BLUE;
+//        }
+//  
+//        g_leds.setPixelColor(row, col, color);
+//      }
+//    }
+//    g_leds.endUpdate();
+//  }
+//} g_boardTest;
+//
+//class BoardTest
+//{
+//  void setup()
+//  {
+//    g_buttons.setNumStates(2);
+//  }
+//
+//  // Basic board test -- lights go on when the button is pressed and go off
+//  // when they are pressed again.
+//  void loop()
+//  {
+//    g_leds.beginUpdate();
+//    // Update our lights.
+//    for (uint8_t row = 0; row < NUM_ROWS; row++)
+//    {
+//      for (uint8_t col = 0; col < NUM_COLS; col++)
+//      {
+//        uint32_t color = COLOR_NONE;
+//        
+//        if (g_buttons.isButtonPressed(row,col))
+//        {
+//          color = MAGENTA;
+//        }
+//        else if (g_buttons.isButtonOn(row,col))
+//        {
+//          color = BLUE;
+//        }
+//  
+//        g_leds.setPixelColor(row, col, color);
+//      }
+//    }
+//    g_leds.endUpdate();
+//  }
+//} g_boardTest;
+//
+enum Mode
+{
+  MODE_BOARD_TEST,
+  MODE_DRAWING,
+  MODE_SIMON_SAYS,
+  MODE_LIGHTS_OUT,
+  NUM_MODES
+};
+
+uint8_t g_mode;
+uint32_t lastChange = 0;
+
+void loop()
+{
+//  // light LED as we start reading data
+//  digitalWrite(ledPin, HIGH);
+
+  bool bButtonsChanged = g_buttons.updateButtonState();
+
+//  // turn off LED - done reading data
+//  digitalWrite(ledPin, LOW);
+
+  if (bButtonsChanged)
+  {
+    lastChange = millis();
+    switch(g_mode)
+    {
+    case MODE_BOARD_TEST:
+      g_boardTest.loop();
+      break;
+    case MODE_DRAWING:
+      ++g_mode;
+      // fall through -- NYI
+    case MODE_SIMON_SAYS:
+      ++g_mode;
+      // fall through -- NYI
+    case MODE_LIGHTS_OUT:
+      ++g_mode;
+      // fall through -- NYI
+    default:
+      // shouldn't get here, but should do something reasonable if we do
+      g_mode = MODE_BOARD_TEST;
+      g_boardTest.loop();
+      break;
+    }
+  } else if (g_buttons.isButtonPressed(0,0)) {
+    int32_t now = millis();
+    // If someone long-pressed the 0 button, switch to a new game
+    if ((now - lastChange) > LONG_PRESS_MS) {
+      lastChange = now;
+      if (++g_mode >= NUM_MODES)
       {
-        // Clear handled flag since we're not being pressed any more.
-        buttons[row][col].handled = false;
-
-        if(buttons[row][col].highlight) 
-        {
-          color = ledHighlightColor;
-        }
-        else
-        {
-          color = ledColorNone;
-        }
+        g_mode = 0; // using 0 instead of the name, since this is easier if the order changes
       }
 
-      int neopixelIndex = colRowToNeopixelIndex(col, row);
-      strip.setPixelColor(neopixelIndex, color);
+      switch(g_mode)
+      {
+      case MODE_BOARD_TEST:
+        g_boardTest.setup();
+        break;
+      case MODE_DRAWING:
+        ++g_mode;
+        // fall through -- NYI
+      case MODE_SIMON_SAYS:
+        ++g_mode;
+        // fall through -- NYI
+      case MODE_LIGHTS_OUT:
+        ++g_mode;
+        // fall through -- NYI
+      default:
+        // shouldn't get here, but should do something reasonable if we do
+        g_mode = MODE_BOARD_TEST;
+        g_boardTest.setup();
+        break;
+      }
     }
   }
-  strip.show();
 }
 
-uint32_t Wheel(byte WheelPos) {
-  if(WheelPos < 85) {
-   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-  } else if(WheelPos < 170) {
-   WheelPos -= 85;
-   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  } else {
-   WheelPos -= 170;
-   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-}
+
+//
+//uint32_t Wheel(byte WheelPos) {
+//  if(WheelPos < 85) {
+//   return strip.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
+//  } else if(WheelPos < 170) {
+//   WheelPos -= 85;
+//   return strip.Color(255 - WheelPos * 3, 0, WheelPos * 3);
+//  } else {
+//   WheelPos -= 170;
+//   return strip.Color(0, WheelPos * 3, 255 - WheelPos * 3);
+//  }
+//}
 
 void rainbow(uint8_t wait) {
-  uint16_t i, j;
-  for(j=0; j<256; j++) {
-    for(i=0; i<strip.numPixels(); i++) {
-      strip.setPixelColor(i, Wheel((i+j) & 255));
-    }
-    strip.show();
+  uint8_t i = 0;
+
+  // ramp up (green)
+  do
+  {
+    uint32_t color = (uint32_t)i << 8; // start up green.
+    g_leds.setAllPixels(color);
+//    delay(1); // minimum delay on start, since this is just ramping the leds up.
+  } while (++i > 0);
+
+  // green-red
+  do
+  {
+    uint32_t color = (uint32_t)(255 - i) << 8 | (uint32_t)i << 16; // cycle green - red.
+    g_leds.setAllPixels(color);
     delay(wait);
-  }
+  } while (++i > 0);
+
+  // red-blue
+  do
+  {
+    uint32_t color = (uint32_t)(255 - i) << 16 | (uint32_t)i; // cycle red - blue.
+    g_leds.setAllPixels(color);
+    delay(wait);
+  } while (++i > 0);
+
+  // blue-green
+  do
+  {
+    uint32_t color = (uint32_t)(255 - i) | (uint32_t)i << 8; // cycle blue - green.
+    g_leds.setAllPixels(color);
+    delay(wait);
+  } while (++i > 0);
+
+  // ramp down (green)
+  do
+  {
+    uint32_t color = 255 - (uint32_t)i << 8; // fade up green.
+    g_leds.setAllPixels(color);
+//    delay(1); // minimum delay on start, since this is just ramping the leds up.
+  } while (++i > 0);
 }
 
 
